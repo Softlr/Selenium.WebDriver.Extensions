ade5c7beed85baf0b9e4ec589cbdf3fdc178b1c5
Lukasz Rajchel <lukasz@rajchel.pl>
2014-07-07 11:57:09 +0200

    build settings updated

--- go.bat
+++ go.bat
@@ -1,4 +1,4 @@
 @src\.nuget\nuget install src/.nuget/packages.config -o src/packages
-@src\.nuget\nuget restore src/SeleniumHelpers.sln
+@src\.nuget\nuget restore src/Selenium.WebDriver.Extensions.sln
 @xcopy /Y src\.nuget\NAnt.exe.config src\packages\NAnt.Portable.0.92\
-@src\packages\NAnt.Portable.0.92\NAnt -buildfile:SeleniumHelpers.build %*
+@src\packages\NAnt.Portable.0.92\NAnt -buildfile:Selenium.WebDriver.Extensions.build %*
