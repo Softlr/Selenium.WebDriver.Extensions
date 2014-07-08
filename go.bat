020c4232d9ae1ccd66dc09687042880e80044d1a
Lukasz Rajchel <lukasz@rajchel.pl>
2014-07-08 07:45:43 +0200

    project organization changes

--- go.bat
+++ go.bat
@@ -1,4 +1,4 @@
-@src\.nuget\nuget install src/.nuget/packages.config -o src/packages
-@src\.nuget\nuget restore src/Selenium.WebDriver.Extensions.sln
-@xcopy /Y src\.nuget\NAnt.exe.config src\packages\NAnt.Portable.0.92\
-@src\packages\NAnt.Portable.0.92\NAnt -buildfile:Selenium.WebDriver.Extensions.build %*
+@.nuget\nuget install .nuget/packages.config -o packages
+@.nuget\nuget restore Selenium.WebDriver.Extensions.sln
+@xcopy /Y .nuget\NAnt.exe.config packages\NAnt.Portable.0.92\
+@packages\NAnt.Portable.0.92\NAnt -buildfile:Selenium.WebDriver.Extensions.build %*
