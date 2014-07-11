8e4469e2c7c5624a50c22a6856ef961c71b7e533
Lukasz Rajchel <lukasz@rajchel.pl>
2014-07-11 11:45:07 +0200

    missing file added

---
+++ src/Selenium.WebDriver.Extensions/Position.cs
@@ -0,0 +1,29 @@
+﻿namespace Selenium.WebDriver.Extensions
+{
+    /// <summary>
+    /// The position of DOM element on a page.
+    /// </summary>
+    public class Position
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Position"/> class.
+        /// </summary>
+        /// <param name="top">The top coordinate of DOM element position.</param>
+        /// <param name="left">The left coordinate of DOM element position.</param>
+        public Position(int top, int left)
+        {
+            this.Top = top;
+            this.Left = left;
+        }
+        
+        /// <summary>
+        /// Gets the top coordinate of DOM element position.
+        /// </summary>
+        public int Top { get; private set; }
+
+        /// <summary>
+        /// Gets the left coordinate of a DOM element position.
+        /// </summary>
+        public int Left { get; private set; }
+    }
+}
