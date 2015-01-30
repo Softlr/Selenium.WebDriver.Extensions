5657d524a76a85aecd47ef54a035b07da150d2f8
Lukasz Rajchel <lukasz@rajchel.pl>
2015-01-30 12:31:54 +0100

    added unit tests for query selector

---
+++ test/Selenium.WebDriver.Extensions.Tests/QuerySelectorTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Selenium.WebDriver.Extensions.Tests
+{
+    using System;
+    using System.Collections;
+    using System.Diagnostics.CodeAnalysis;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Query selector tests.
+    /// </summary>
+    [TestFixture]
+    [Category("Unit Tests")]
+    [ExcludeFromCodeCoverage]
+    public class QuerySelectorTests
+    {
+        /// <summary>
+        /// Gets the selector test cases.
+        /// </summary>
+        private static IEnumerable SelectorTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(By.QuerySelector("div"))
+                    .Returns("document.querySelectorAll('div')").SetName("document.querySelectorAll('div')");
+                yield return new TestCaseData(By.QuerySelector("input[type='text']"))
+                    .Returns("document.querySelectorAll('input[type=\"text\"]')").SetName("escape single quotes");
+            }
+        }
+
+        /// <summary>
+        /// Tests if the proper selector is generated.
+        /// </summary>
+        /// <param name="selector">The query selector.</param>
+        /// <returns>The generated query selector.</returns>
+        [TestCaseSource("SelectorTestCases")]
+        public string Selector(QuerySelector selector)
+        {
+            Assert.AreEqual(selector.Selector, selector.ToString());
+            return selector.Selector;
+        }
+
+        /// <summary>
+        /// Tests if the null selector is handled properly.
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullSelector()
+        {
+            By.QuerySelector(null);
+        }
+    }
+}
