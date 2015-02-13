969932111c68e0d677989b06daacbee7a09adee1
Lukasz Rajchel <lukasz@rajchel.pl>
2015-02-13 08:20:36 +0100

    new tests added

---
+++ test/Selenium.WebDriver.Extensions.Tests/Models/PositionTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Selenium.WebDriver.Extensions.Tests.Models
+{
+    using System.Diagnostics.CodeAnalysis;
+    using NUnit.Framework;
+    using Selenium.WebDriver.Extensions.Models;
+
+    [TestFixture]
+    [Category("Unit Tests")]
+    [ExcludeFromCodeCoverage]
+    public class PositionTests
+    {
+        /// <summary>
+        /// Tests hash code generation.
+        /// </summary>
+        [Test]
+        public void HashCodeTest()
+        {
+            var position1 = new Position(0, 0);
+            var position2 = new Position(0, 1);
+            var position3 = new Position(0, 0);
+            var position4 = new Position(1, 0);
+
+            Assert.IsFalse(position1.Equals(position2));
+            Assert.IsTrue(position1.Equals(position3));
+            Assert.IsFalse(position1.Equals(position4));
+
+            Assert.IsFalse(position1.Equals((object)position2));
+            Assert.IsTrue(position1.Equals((object)position3));
+            Assert.IsFalse(position1.Equals((object)position4));
+            Assert.IsFalse(position1.Equals(new object()));
+            
+            Assert.AreNotEqual(position1.GetHashCode(), position2.GetHashCode());
+            Assert.AreEqual(position1.GetHashCode(), position3.GetHashCode());
+            Assert.AreNotEqual(position1.GetHashCode(), position4.GetHashCode());
+        }
+    }
+}
