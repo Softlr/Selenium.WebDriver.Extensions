b96409f6e5a32737a50210a7415f397b1c0d8b41
Lukasz Rajchel <lukasz@rajchel.pl>
2015-02-17 08:27:49 +0100

    restoring all nuget packages in one step

--- go.bat
+++ go.bat
@@ -1,4 +1,3 @@
-@.nuget\nuget install .nuget/packages.config -o packages
-@.nuget\nuget restore Selenium.WebDriver.Extensions.sln
+@.nuget\nuget restore
 @xcopy /Y .nuget\NAnt.exe.config packages\NAnt.Portable.0.92\
 @packages\NAnt.Portable.0.92\NAnt -buildfile:Selenium.WebDriver.Extensions.build %*
