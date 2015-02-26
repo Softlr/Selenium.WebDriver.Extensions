59cf4cb3bd6e0052492d0568cc2e77ad8bb82434
Lukasz Rajchel <lukasz@rajchel.pl>
2015-02-26 11:29:04 +0100

    all integration tests running also on Chrome

--- test/Selenium.WebDriver.Extensions.IntegrationTests/WebDriverExtensions.JQuerySelectorTests.cs
+++ test/Selenium.WebDriver.Extensions.IntegrationTests/WebDriverExtensions.JQuerySelectorTests.cs
@@ -19,7 +19,13 @@
         "PhantomJS",
         "https://cdn.rawgit.com/RaYell/selenium-webdriver-extensions/642465fff703167db9516f24330f8413916524e5/test/Selenium.WebDriver.Extensions.IntegrationTests/TestCases/JQuery/Loaded.html")]
     [TestFixture(
+        "Chrome",
+        "https://cdn.rawgit.com/RaYell/selenium-webdriver-extensions/642465fff703167db9516f24330f8413916524e5/test/Selenium.WebDriver.Extensions.IntegrationTests/TestCases/JQuery/Loaded.html")]
+    [TestFixture(
         "PhantomJS", 
+        "https://cdn.rawgit.com/RaYell/selenium-webdriver-extensions/642465fff703167db9516f24330f8413916524e5/test/Selenium.WebDriver.Extensions.IntegrationTests/TestCases/JQuery/Unloaded.html")]
+    [TestFixture(
+        "Chrome",
         "https://cdn.rawgit.com/RaYell/selenium-webdriver-extensions/642465fff703167db9516f24330f8413916524e5/test/Selenium.WebDriver.Extensions.IntegrationTests/TestCases/JQuery/Unloaded.html")]
     [Category("Integration Tests")]
 #if !NET35
