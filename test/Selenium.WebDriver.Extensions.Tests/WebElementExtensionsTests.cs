caf9cabf63896eacc68884610c508ad6936d8dc6
Lukasz Rajchel <lukasz@rajchel.pl>
2015-02-26 09:09:55 +0100

    more unit tests added

--- test/Selenium.WebDriver.Extensions.Tests/WebElementExtensionsTests.cs
+++ test/Selenium.WebDriver.Extensions.Tests/WebElementExtensionsTests.cs
@@ -6,6 +6,7 @@
     using NUnit.Framework;
     using OpenQA.Selenium;
     using Selenium.WebDriver.Extensions.JQuery;
+    using Selenium.WebDriver.Extensions.Sizzle;
     using Selenium.WebDriver.Extensions.Shared;
     using By = Selenium.WebDriver.Extensions.By;
     
@@ -106,6 +107,84 @@
             webElement.SetupGet(x => x.WrappedDriver).Returns(driver.Object);
 
             var result = webElement.Object.FindElements(By.JQuerySelector("span"));
+
+            Assert.AreEqual(2, result.Count);
+
+            Assert.AreEqual("span", result[0].TagName);
+            Assert.AreEqual("test1", result[0].GetAttribute("class"));
+
+            Assert.AreEqual("span", result[1].TagName);
+            Assert.AreEqual("test2", result[1].GetAttribute("class"));
+        }
+
+        /// <summary>
+        /// Tests finding an element.
+        /// </summary>
+        [Test]
+        public void FindElementWithSizzle()
+        {
+            var selector = By.SizzleSelector("div");
+
+            var rootElement = new Mock<IWebElement>();
+            rootElement.SetupGet(x => x.TagName).Returns("div");
+
+            var element = new Mock<IWebElement>();
+            element.SetupGet(x => x.TagName).Returns("span");
+
+            var rootList = new List<IWebElement> { rootElement.Object };
+            var driver = MockWebDriver("return Sizzle('div');", new ReadOnlyCollection<IWebElement>(rootList));
+            driver.As<IJavaScriptExecutor>().Setup(x => x.ExecuteScript(It.IsRegex("getDomPath")))
+                .Returns("body > div");
+            var elementList = new List<IWebElement> { element.Object };
+            driver.As<IJavaScriptExecutor>()
+                .Setup(x => x.ExecuteScript("return Sizzle('span', Sizzle('body > div')[0]);"))
+                .Returns(new ReadOnlyCollection<IWebElement>(elementList));
+
+            var webElement = new Mock<WebElement>();
+            webElement.SetupGet(x => x.TagName).Returns("div");
+            webElement.SetupGet(x => x.Selector).Returns(selector);
+            webElement.SetupGet(x => x.WrappedDriver).Returns(driver.Object);
+
+            var result = webElement.Object.FindElement(By.SizzleSelector("span"));
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("span", result.TagName);
+        }
+
+        /// <summary>
+        /// Tests finding elements.
+        /// </summary>
+        [Test]
+        public void FindElementsWithSizzle()
+        {
+            var selector = By.SizzleSelector("div");
+
+            var rootElement = new Mock<IWebElement>();
+            rootElement.SetupGet(x => x.TagName).Returns("div");
+
+            var element1 = new Mock<IWebElement>();
+            element1.Setup(x => x.TagName).Returns("span");
+            element1.Setup(x => x.GetAttribute("class")).Returns("test1");
+
+            var element2 = new Mock<IWebElement>();
+            element2.Setup(x => x.TagName).Returns("span");
+            element2.Setup(x => x.GetAttribute("class")).Returns("test2");
+
+            var rootList = new List<IWebElement> { rootElement.Object };
+            var elementList = new List<IWebElement> { element1.Object, element2.Object };
+            var driver = MockWebDriver("return Sizzle('div');", new ReadOnlyCollection<IWebElement>(rootList));
+            driver.As<IJavaScriptExecutor>().Setup(x => x.ExecuteScript(It.IsRegex("getDomPath")))
+                .Returns("body > div");
+            driver.As<IJavaScriptExecutor>()
+                .Setup(x => x.ExecuteScript("return Sizzle('span', Sizzle('body > div')[0]);"))
+                .Returns(new ReadOnlyCollection<IWebElement>(elementList));
+
+            var webElement = new Mock<WebElement>();
+            webElement.SetupGet(x => x.TagName).Returns("div");
+            webElement.SetupGet(x => x.Selector).Returns(selector);
+            webElement.SetupGet(x => x.WrappedDriver).Returns(driver.Object);
+
+            var result = webElement.Object.FindElements(By.SizzleSelector("span"));
 
             Assert.AreEqual(2, result.Count);
 
