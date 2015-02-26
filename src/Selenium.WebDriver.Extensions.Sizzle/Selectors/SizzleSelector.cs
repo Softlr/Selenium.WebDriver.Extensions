e3934c714940753802204317584b52493b4137df
Lukasz Rajchel <lukasz@rajchel.pl>
2015-02-26 13:32:24 +0100

    equality operator overloads implemented on sizzle selectors

--- src/Selenium.WebDriver.Extensions.Sizzle/Selectors/SizzleSelector.cs
+++ src/Selenium.WebDriver.Extensions.Sizzle/Selectors/SizzleSelector.cs
@@ -61,6 +61,67 @@
         public SizzleSelector Context { get; private set; }
 
         /// <summary>
+        /// Compares two selectors and returns <c>true</c> if they are equal.
+        /// </summary>
+        /// <param name="selector1">The first selector to compare.</param>
+        /// <param name="selector2">The second selector to compare.</param>
+        /// <returns><c>true</c> if the selectors are equal; otherwise, <c>false</c>.</returns>
+        public static bool operator ==(SizzleSelector selector1, SizzleSelector selector2)
+        {
+            if (ReferenceEquals(selector1, selector2))
+            {
+                return true;
+            }
+
+            if (((object)selector1 == null) || ((object)selector2 == null))
+            {
+                return false;
+            }
+
+            return selector1.Equals(selector2);
+        }
+
+        /// <summary>
+        /// Compares two selectors and returns <c>true</c> if they are not equal.
+        /// </summary>
+        /// <param name="selector1">The first selector to compare.</param>
+        /// <param name="selector2">The second selector to compare.</param>
+        /// <returns><c>true</c> if the selectors are not equal; otherwise, <c>false</c>.</returns>
+        public static bool operator !=(SizzleSelector selector1, SizzleSelector selector2)
+        {
+            return !(selector1 == selector2);
+        }
+
+        /// <summary>
+        /// Determines whether two object instances are equal.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object. </param>
+        /// <returns>
+        /// <c>true</c> if the specified object is equal to the current object; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var selector = (SizzleSelector)obj;
+            return this.RawSelector == selector.RawSelector && this.Context == selector.Context;
+        }
+
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
+        public override int GetHashCode()
+        {
+            return this.Context == null
+                ? Tuple.Create(this.RawSelector).GetHashCode()
+                : Tuple.Create(this.RawSelector, this.Context.GetHashCode()).GetHashCode();
+        }
+
+        /// <summary>
         /// Returns a string that represents the current object.
         /// </summary>
         /// <returns>A string that represents the current object.</returns>
