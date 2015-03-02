64fa35302ced6e82f5517f4e7303760c238f3d89
Lukasz Rajchel <lukasz@rajchel.pl>
2015-03-02 13:29:15 +0100

    returning standard selenium selectors from jquery/sizzle/query selector by overrides

--- src/Selenium.WebDriver.Extensions.Sizzle/Models/By.cs
+++ src/Selenium.WebDriver.Extensions.Sizzle/Models/By.cs
@@ -24,5 +24,88 @@
         {
             return new SizzleSelector(selector, context);
         }
+
+        /// <summary>
+        /// Gets a mechanism to find elements by their CSS class.
+        /// </summary>
+        /// <param name="classNameToFind">The CSS class to find.</param>
+        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
+        public static new OpenQA.Selenium.By ClassName(string classNameToFind)
+        {
+            return OpenQA.Selenium.By.ClassName(classNameToFind);
+        }
+
+        /// <summary>
+        /// Gets a mechanism to find elements by their cascading style sheet (CSS) selector.
+        /// </summary>
+        /// <param name="cssSelectorToFind">The CSS selector to find.</param>
+        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
+        public static new OpenQA.Selenium.By CssSelector(string cssSelectorToFind)
+        {
+            return OpenQA.Selenium.By.CssSelector(cssSelectorToFind);
+        }
+
+        /// <summary>
+        /// Gets a mechanism to find elements by their ID.
+        /// </summary>
+        /// <param name="idToFind">The ID to find.</param>
+        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
+        public static new OpenQA.Selenium.By Id(string idToFind)
+        {
+            return OpenQA.Selenium.By.Id(idToFind);
+        }
+
+        /// <summary>
+        /// Gets a mechanism to find elements by their link text.
+        /// </summary>
+        /// <param name="linkTextToFind">The link text to find.</param>
+        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
+        public static new OpenQA.Selenium.By LinkText(string linkTextToFind)
+        {
+            return OpenQA.Selenium.By.LinkText(linkTextToFind);
+        }
+
+        /// <summary>
+        /// Gets a mechanism to find elements by their name.
+        /// </summary>
+        /// <param name="nameToFind">The name to find.</param>
+        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
+        public static new OpenQA.Selenium.By Name(string nameToFind)
+        {
+            return OpenQA.Selenium.By.Name(nameToFind);
+        }
+
+        /// <summary>
+        /// Gets a mechanism to find elements by a partial match on their link text.
+        /// </summary>
+        /// <param name="partialLinkTextToFind">The partial link text to find.</param>
+        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
+        public static new OpenQA.Selenium.By PartialLinkText(string partialLinkTextToFind)
+        {
+            return OpenQA.Selenium.By.PartialLinkText(partialLinkTextToFind);
+        }
+
+        /// <summary>
+        /// Gets a mechanism to find elements by their tag name.
+        /// </summary>
+        /// <param name="tagNameToFind">The tag name to find.</param>
+        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
+        public static new OpenQA.Selenium.By TagName(string tagNameToFind)
+        {
+            return OpenQA.Selenium.By.TagName(tagNameToFind);
+        }
+
+        /// <summary>
+        /// Gets a mechanism to find elements by an XPath query. When searching within a WebElement using xpath be 
+        /// aware that WebDriver follows standard conventions: a search prefixed with "//" will search the entire 
+        /// document, not just the children of this current node.  Use ".//" to limit your search to the children of 
+        /// this WebElement.
+        /// </summary>
+        /// <param name="xpathToFind">The XPath query to use.</param>
+        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
+        public static new OpenQA.Selenium.By XPath(string xpathToFind)
+        {
+            return OpenQA.Selenium.By.XPath(xpathToFind);
+        }
     }
 }
--- test/Selenium.WebDriver.Extensions.QuerySelector.Tests/Models/ByTests.cs
+++ test/Selenium.WebDriver.Extensions.QuerySelector.Tests/Models/ByTests.cs
@@ -13,7 +13,6 @@
 #endif
     public class ByTests
     {
-        /*
         /// <summary>
         /// Tests the class name.
         /// </summary>
@@ -21,9 +20,10 @@
         public void ClassName()
         {
             const string ClassName = "test";
+            var selector = OpenQA.Selenium.By.ClassName(ClassName);
             var wrappedBy = By.ClassName(ClassName);
 
-            Assert.AreEqual(".test", wrappedBy.RawSelector);
+            Assert.AreEqual(selector, wrappedBy);
         }
 
         /// <summary>
@@ -33,9 +33,10 @@
         public void CssSelector()
         {
             const string CssSelector = "div.test";
+            var selector = OpenQA.Selenium.By.CssSelector(CssSelector);
             var wrappedBy = By.CssSelector(CssSelector);
 
-            Assert.AreEqual(CssSelector, wrappedBy.RawSelector);
+            Assert.AreEqual(selector, wrappedBy);
         }
 
         /// <summary>
@@ -45,9 +46,10 @@
         public void Id()
         {
             const string Id = "test";
+            var selector = OpenQA.Selenium.By.Id(Id);
             var wrappedBy = By.Id(Id);
 
-            Assert.AreEqual("#test", wrappedBy.RawSelector);
+            Assert.AreEqual(selector, wrappedBy);
         }
 
         /// <summary>
@@ -57,21 +59,10 @@
         public void LinkText()
         {
             const string LinkText = "test";
+            var selector = OpenQA.Selenium.By.LinkText(LinkText);
             var wrappedBy = By.LinkText(LinkText);
 
-            Assert.AreEqual("test", wrappedBy.RawSelector);
-        }
-
-        /// <summary>
-        /// Tests the link text.
-        /// </summary>
-        [Test]
-        public void LinkTextWithBaseElement()
-        {
-            const string LinkText = "test";
-            var wrappedBy = By.LinkText(LinkText, "document");
-
-            Assert.AreEqual("test", wrappedBy.RawSelector);
+            Assert.AreEqual(selector, wrappedBy);
         }
 
         /// <summary>
@@ -81,9 +72,10 @@
         public void Name()
         {
             const string Name = "test";
+            var selector = OpenQA.Selenium.By.Name(Name);
             var wrappedBy = By.Name(Name);
 
-            Assert.AreEqual("[name='test']", wrappedBy.RawSelector);
+            Assert.AreEqual(selector, wrappedBy);
         }
 
         /// <summary>
@@ -93,21 +85,10 @@
         public void PartialLinkText()
         {
             const string PartialLinkText = "test";
+            var selector = OpenQA.Selenium.By.PartialLinkText(PartialLinkText);
             var wrappedBy = By.PartialLinkText(PartialLinkText);
 
-            Assert.AreEqual("test", wrappedBy.RawSelector);
-        }
-
-        /// <summary>
-        /// Tests the partial link text.
-        /// </summary>
-        [Test]
-        public void PartialLinkTextWithBaseElement()
-        {
-            const string PartialLinkText = "test";
-            var wrappedBy = By.PartialLinkText(PartialLinkText, "document");
-
-            Assert.AreEqual("test", wrappedBy.RawSelector);
+            Assert.AreEqual(selector, wrappedBy);
         }
 
         /// <summary>
@@ -117,9 +98,10 @@
         public void TagName()
         {
             const string TagName = "div";
+            var selector = OpenQA.Selenium.By.TagName(TagName);
             var wrappedBy = By.TagName(TagName);
 
-            Assert.AreEqual(TagName, wrappedBy.RawSelector);
+            Assert.AreEqual(selector, wrappedBy);
         }
 
         /// <summary>
@@ -129,9 +111,10 @@
         public void XPath()
         {
             const string XPath = "/body/div";
+            var selector = OpenQA.Selenium.By.XPath(XPath);
             var wrappedBy = By.XPath(XPath);
 
-            Assert.AreEqual(XPath, wrappedBy.RawSelector);
-        }*/
+            Assert.AreEqual(selector, wrappedBy);
+        }
     }
 }
