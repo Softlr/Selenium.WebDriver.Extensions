66b55f7eb304006d27fde0cfbd19a2db5e62d83e
Lukasz Rajchel <lukasz@rajchel.pl>
2015-03-02 08:02:11 +0100

    shared tests project added

---
+++ test/Selenium.WebDriver.Extensions.Shared.Tests/Selectors/LinkTextSelectorTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Selenium.WebDriver.Extensions.Shared.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Link text selector tests.
+    /// </summary>
+    [TestFixture]
+    [Category("Unit Tests")]
+#if !NET35
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+#endif
+    public class LinkTextSelectorTests
+    {
+        /// <summary>
+        /// Tests if the null selector is handled properly.
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullBaseElementSelector()
+        {
+            By.LinkText("test", null);
+        }
+    }
+}
