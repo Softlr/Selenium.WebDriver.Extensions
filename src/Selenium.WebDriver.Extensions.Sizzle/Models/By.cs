d48f3481bc21c46a6ee0be45cbbc29051def1669
Lukasz Rajchel <lukasz@rajchel.pl>
2015-03-04 10:27:19 +0100

    jquery/sizzle project changed to depend on core project

--- src/Selenium.WebDriver.Extensions.Sizzle/Models/By.cs
+++ src/Selenium.WebDriver.Extensions.Sizzle/Models/By.cs
@@ -1,7 +1,9 @@
 ﻿namespace Selenium.WebDriver.Extensions.Sizzle
 {
     using JetBrains.Annotations;
-    
+    using Selenium.WebDriver.Extensions.Core;
+    using Selenium.WebDriver.Extensions.Shared;
+
     /// <summary>
     /// Extends the selenium <see cref="OpenQA.Selenium.By"/> additional selectors to be used.
     /// </summary>
@@ -26,73 +28,129 @@
         }
 
         /// <summary>
+        /// Gets a mechanism to find elements matching JavaScript query selector.
+        /// </summary>
+        /// <param name="selector">A string containing a selector expression</param>
+        /// <param name="baseElement">
+        /// A string defining the base element on which base element the selector should be invoked.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Selenium.WebDriver.Extensions.Core.QuerySelector"/> object the driver can use to find the 
+        /// elements.
+        /// </returns>
+        public static QuerySelector QuerySelector(string selector, string baseElement = "document")
+        {
+            return Core.By.QuerySelector(selector, baseElement);
+        }
+
+        /// <summary>
+        /// Gets a mechanism to find elements matching JavaScript query selector.
+        /// </summary>
+        /// <param name="selector">A string containing a selector expression</param>
+        /// <param name="baseSelector">A query selector on which defines a base element for the new selector.</param>
+        /// <returns>
+        /// A <see cref="Selenium.WebDriver.Extensions.Core.QuerySelector"/> object the driver can use to find the 
+        /// elements.
+        /// </returns>
+        public static QuerySelector QuerySelector(string selector, ISelector baseSelector)
+        {
+            return Core.By.QuerySelector(selector, baseSelector);
+        }
+
+        /// <summary>
         /// Gets a mechanism to find elements by their CSS class.
         /// </summary>
         /// <param name="classNameToFind">The CSS class to find.</param>
-        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
-        public static new OpenQA.Selenium.By ClassName(string classNameToFind)
+        /// <returns>A <see cref="ClassNameSelector"/> object the driver can use to find the elements.</returns>
+        public static new ClassNameSelector ClassName(string classNameToFind)
         {
-            return OpenQA.Selenium.By.ClassName(classNameToFind);
+            return Core.By.ClassName(classNameToFind);
         }
 
         /// <summary>
         /// Gets a mechanism to find elements by their cascading style sheet (CSS) selector.
         /// </summary>
         /// <param name="cssSelectorToFind">The CSS selector to find.</param>
-        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
-        public static new OpenQA.Selenium.By CssSelector(string cssSelectorToFind)
+        /// <returns>A <see cref="CssSelector"/> object the driver can use to find the elements.</returns>
+        public static new CssSelector CssSelector(string cssSelectorToFind)
         {
-            return OpenQA.Selenium.By.CssSelector(cssSelectorToFind);
+            return Core.By.CssSelector(cssSelectorToFind);
         }
 
         /// <summary>
         /// Gets a mechanism to find elements by their ID.
         /// </summary>
         /// <param name="idToFind">The ID to find.</param>
-        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
-        public static new OpenQA.Selenium.By Id(string idToFind)
+        /// <returns>An <see cref="IdSelector"/> object the driver can use to find the elements.</returns>
+        public static new IdSelector Id(string idToFind)
         {
-            return OpenQA.Selenium.By.Id(idToFind);
+            return Core.By.Id(idToFind);
         }
 
         /// <summary>
         /// Gets a mechanism to find elements by their link text.
         /// </summary>
         /// <param name="linkTextToFind">The link text to find.</param>
-        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
-        public static new OpenQA.Selenium.By LinkText(string linkTextToFind)
+        /// <returns>A <see cref="LinkTextSelector"/> object the driver can use to find the elements.</returns>
+        public static new LinkTextSelector LinkText(string linkTextToFind)
         {
-            return OpenQA.Selenium.By.LinkText(linkTextToFind);
+            return Core.By.LinkText(linkTextToFind);
+        }
+
+        /// <summary>
+        /// Gets a mechanism to find elements by their link text.
+        /// </summary>
+        /// <param name="linkTextToFind">The link text to find.</param>
+        /// <param name="baseElement">
+        /// A string defining the base element on which base element the selector should be invoked.
+        /// </param>
+        /// <returns>A <see cref="LinkTextSelector"/> object the driver can use to find the elements.</returns>
+        public static LinkTextSelector LinkText(string linkTextToFind, string baseElement)
+        {
+            return Core.By.LinkText(linkTextToFind, baseElement);
         }
 
         /// <summary>
         /// Gets a mechanism to find elements by their name.
         /// </summary>
         /// <param name="nameToFind">The name to find.</param>
-        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
-        public static new OpenQA.Selenium.By Name(string nameToFind)
+        /// <returns>A <see cref="NameSelector"/> object the driver can use to find the elements.</returns>
+        public static new NameSelector Name(string nameToFind)
         {
-            return OpenQA.Selenium.By.Name(nameToFind);
+            return Core.By.Name(nameToFind);
         }
 
         /// <summary>
         /// Gets a mechanism to find elements by a partial match on their link text.
         /// </summary>
         /// <param name="partialLinkTextToFind">The partial link text to find.</param>
-        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
-        public static new OpenQA.Selenium.By PartialLinkText(string partialLinkTextToFind)
+        /// <returns>A <see cref="PartialLinkTextSelector"/> object the driver can use to find the elements.</returns>
+        public static new PartialLinkTextSelector PartialLinkText(string partialLinkTextToFind)
         {
-            return OpenQA.Selenium.By.PartialLinkText(partialLinkTextToFind);
+            return Core.By.PartialLinkText(partialLinkTextToFind);
+        }
+
+        /// <summary>
+        /// Gets a mechanism to find elements by a partial match on their link text.
+        /// </summary>
+        /// <param name="partialLinkTextToFind">The partial link text to find.</param>
+        /// <param name="baseElement">
+        /// A string defining the base element on which base element the selector should be invoked.
+        /// </param>
+        /// <returns>A <see cref="PartialLinkTextSelector"/> object the driver can use to find the elements.</returns>
+        public static PartialLinkTextSelector PartialLinkText(string partialLinkTextToFind, string baseElement)
+        {
+            return Core.By.PartialLinkText(partialLinkTextToFind, baseElement);
         }
 
         /// <summary>
         /// Gets a mechanism to find elements by their tag name.
         /// </summary>
         /// <param name="tagNameToFind">The tag name to find.</param>
-        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
-        public static new OpenQA.Selenium.By TagName(string tagNameToFind)
+        /// <returns>A <see cref="TagNameSelector"/> selector object the driver can use to find the elements.</returns>
+        public static new TagNameSelector TagName(string tagNameToFind)
         {
-            return OpenQA.Selenium.By.TagName(tagNameToFind);
+            return Core.By.TagName(tagNameToFind);
         }
 
         /// <summary>
@@ -102,10 +160,10 @@
         /// this WebElement.
         /// </summary>
         /// <param name="xpathToFind">The XPath query to use.</param>
-        /// <returns>A <see cref="OpenQA.Selenium.By"/> object the driver can use to find the elements.</returns>
-        public static new OpenQA.Selenium.By XPath(string xpathToFind)
+        /// <returns>A <see cref="XPathSelector"/> object the driver can use to find the elements.</returns>
+        public static new XPathSelector XPath(string xpathToFind)
         {
-            return OpenQA.Selenium.By.XPath(xpathToFind);
+            return Core.By.XPath(xpathToFind);
         }
     }
 }
