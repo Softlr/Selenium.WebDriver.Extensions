ed89238e5d3750f7c76668e02dc29a7c429f9993
Lukasz Rajchel <lukasz@rajchel.pl>
2015-03-20 14:19:15 +0100

    added descriptions for the tasks

--- psake/default.ps1
+++ psake/default.ps1
@@ -8,114 +8,112 @@ properties {
 
 FormatTaskName '-------- {0} --------'
 
-task default -depends Compile, Test, Coverage, Docs, Pack
+Task default -Depends Compile, Test, Coverage, Docs, Pack
 
-task CleanNet45 {
+Task CleanNet45 -Description "Cleans the output directory of the default .NET 4.5 build configuration" {
 	. .\msbuild.ps1
 	Invoke-Build $solution -Target Clean
 }
 
-task CleanNet40 {
+Task CleanNet40 -Description "Cleans the output directory of the .NET 4.0 build configuration" {
 	. .\msbuild.ps1
 	Invoke-Build $solution -BuildConfiguration Release-Net40 -Target Clean
 }
 
-task CleanNet35 {
+Task CleanNet35 -Description "Cleans the output directory of the .NET 3.5 build configuration" {
 	. .\msbuild.ps1
 	Invoke-Build $solution -BuildConfiguration Release-Net35 -Target Clean
 }
 
-task CleanDocs {
+Task CleanDocs -Description "Cleans the output directory of the documentation build configuration" {
 	. .\msbuild.ps1
 	Invoke-Build $solution -BuildConfiguration Docs -Target Clean
 }
 
-task Clean -depends CleanNet45, CleanNet40, CleanNet35, CleanDocs
+Task Clean -Description "Cleans the output directory of all build configurations" -Depends CleanNet45, CleanNet40, CleanNet35, CleanDocs
 
-task CompileNet45 -depends CleanNet45 {
+Task CompileNet45 -Description "Compiles the default .NET 4.5 build configuration" -Depends CleanNet45 {
 	. .\msbuild.ps1
 	Invoke-Build $solution
 }
 
-task CompileNet40 -depends CleanNet40 {
+Task CompileNet40 -Description "Compiles the .NET 4.0 build configuration" -Depends CleanNet40 {
 	. .\msbuild.ps1
 	Invoke-Build $solution -BuildConfiguration Release-Net40
 }
 
-task CompileNet35 -depends CleanNet35 {
+Task CompileNet35 -Description "Compiles the .NET 3.5 build configuration" -Depends CleanNet35 {
 	. .\msbuild.ps1
 	Invoke-Build $solution -BuildConfiguration Release-Net35
 }
 
-task Compile -depends CompileNet45, CompileNet40, CompileNet35
+Task Compile -Description "Compiles all of the build configurations" -Depends CompileNet45, CompileNet40, CompileNet35
 
-task CompileDocs -depends CleanDocs {
+Task Docs  -Description "Compiles the documentation build configuration" -Depends CleanDocs {
 	. .\msbuild.ps1
 	Invoke-Build $solution -BuildConfiguration Docs
 }
 
-task Docs -depends CompileDocs
-
-task Test -depends CompileNet45 {
+Task Test -Description "Runs the unit tests" -Depends CompileNet45 {
 	. .\test.ps1
 	Invoke-Tests $unitTests
 }
 
-task IntegrationPhantomJs -depends CompileNet45 {
+Task IntegrationPhantomJs -Description "Runs the PhantomJS integration tests" -Depends CompileNet45 {
 	. .\test.ps1
 	Invoke-Tests @($integrationTests) -Trait Browser=PhantomJS
 }
 
-task IntegrationChrome -depends CompileNet45 {
+Task IntegrationChrome -Description "Runs the Chrome integration tests" -Depends CompileNet45 {
 	. .\test.ps1
 	Invoke-Tests @($integrationTests) -Trait Browser=Chrome
 }
 
-task IntegrationFirefox -depends CompileNet45 {
+Task IntegrationFirefox -Description "Runs the Firefox integration tests" -Depends CompileNet45 {
 	. .\test.ps1
 	Invoke-Tests @($integrationTests) -Trait Browser=Firefox
 }
 
-task IntegrationInternetExplorer -depends CompileNet45 {
+Task IntegrationInternetExplorer -Description "Runs the Internet Explorer integration tests" -Depends CompileNet45 {
 	. .\test.ps1
 	Invoke-Tests @($integrationTests) -Trait Browser=InternetExplorer
 }
 
-task Integration -depends IntegrationPhantomJs, IntegrationChrome, IntegrationFirefox, IntegrationInternetExplorer
+Task Integration  -Description "Runs all of the integration tests" -Depends IntegrationPhantomJs, IntegrationChrome, IntegrationFirefox, IntegrationInternetExplorer
 
-task AnalyzeCoverage -depends CompileNet45 {
+Task AnalyzeCoverage -Description "Analyzes the code coverage" -Depends CompileNet45 {
 	. .\coverage.ps1
 	Invoke-AnalyzeCoverage $unitTests -Output $coverageXml
 }
 
-task Coverage -depends AnalyzeCoverage {
+Task Coverage -Description "Generates the code coverage HTML report" -Depends AnalyzeCoverage {
 	. .\coverageReport.ps1
 	Invoke-CoverageReportGenerator $coverageXml -Output ..\CoverageReport
 }
 
-task Coveralls -depends AnalyzeCoverage {
+Task Coveralls -Description "Sends coverage data to coveralls.io" -Depends AnalyzeCoverage {
 	. .\coveralls.ps1
 	Send-Coveralls $coverageXml
 }
 
-task PackJQuery -depends Compile {
+Task PackJQuery -Description "Packs JQuery module NuGet package" -Depends Compile {
 	. .\nugetPack.ps1
 	Write-NugetPackage ..\src\Selenium.WebDriver.Extensions.JQuery\Selenium.WebDriver.Extensions.JQuery.nuspec -Version $version -Output ..
 }
 
-task PackSizzle -depends Compile {
+Task PackSizzle -Description "Packs Sizzle module NuGet package" -Depends Compile {
 	. .\nugetPack.ps1
 	Write-NugetPackage ..\src\Selenium.WebDriver.Extensions.Sizzle\Selenium.WebDriver.Extensions.Sizzle.nuspec -Version $version -Output ..
 }
 
-task PackCore -depends Compile {
+Task PackCore -Description "Packs core module NuGet package" -Depends Compile {
 	. .\nugetPack.ps1
 	Write-NugetPackage ..\src\Selenium.WebDriver.Extensions.Core\Selenium.WebDriver.Extensions.Core.nuspec -Version $version -Output ..
 }
 
-task PackCombined -depends Compile {
+Task PackCombined -Description "Packs combined module NuGet package" -Depends Compile {
 	. .\nugetPack.ps1
 	Write-NugetPackage ..\src\Selenium.WebDriver.Extensions\Selenium.WebDriver.Extensions.nuspec -Version $version -Output ..
 }
 
-task Pack -depends PackJQuery, PackSizzle, PackCore, PackCombined
+Task Pack -Description "Packs all of the module NuGet packages" -Depends PackJQuery, PackSizzle, PackCore, PackCombined
