6636cd2efb306fe89785c700bbfed43145b32ccb
Lukasz Rajchel <lukasz@rajchel.pl>
2015-03-23 12:43:35 +0100

    more unit tests added

---
+++ test/Selenium.WebDriver.Extensions.JQuery.Tests/Models/JQueryHelperTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Selenium.WebDriver.Extensions.JQuery.Tests
+{
+    using System;
+    using Moq;
+    using OpenQA.Selenium;
+    using Xunit;
+
+    [Trait("Category", "Unit")]
+#if !NET35
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+#endif
+    public class JQueryHelperTests
+    {
+        [Fact]
+        public void ShouldThrowExceptionWhenCreatingHelperWithNullDriver()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new JQueryHelper(null));
+            Assert.Equal("driver", ex.ParamName);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenLoadingJQueryOfNullVersion()
+        {
+            var driver = new Mock<IWebDriver>();
+            var helper = new JQueryHelper(driver.Object);
+            var ex = Assert.Throws<ArgumentNullException>(() => helper.Load((string)null));
+            Assert.Equal("version", ex.ParamName);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenLoadingJQueryOfEmptyVersion()
+        {
+            var driver = new Mock<IWebDriver>();
+            var helper = new JQueryHelper(driver.Object);
+            var ex = Assert.Throws<ArgumentException>(() => helper.Load(string.Empty));
+            Assert.Equal("version", ex.ParamName);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenLoadingJQueryOfWhiteSpaceOnlyVersion()
+        {
+            var driver = new Mock<IWebDriver>();
+            var helper = new JQueryHelper(driver.Object);
+            var ex = Assert.Throws<ArgumentException>(() => helper.Load(" "));
+            Assert.Equal("version", ex.ParamName);
+        }
+    }
+}
