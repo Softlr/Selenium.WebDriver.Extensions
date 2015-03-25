9ff6a6ce35eab9f248270bcbd2d7609786caee90
Lukasz Rajchel <lukasz@rajchel.pl>
2015-03-25 17:34:15 +0100

    using named parameters instead of positional ones in psake tasks

--- default.ps1
+++ default.ps1
@@ -11,15 +11,15 @@ FormatTaskName '-------- {0} --------'
 Task default -Depends Compile, Test, Coverage, Docs, Pack
 
 Task CleanNet45 -Description "Cleans the output directory of the default .NET 4.5 build configuration" {
-    New-Build $solution -Target Clean
+    New-Build -Solution $solution -Target Clean
 }
 
 Task CleanNet40 -Description "Cleans the output directory of the .NET 4.0 build configuration" {
-    New-Build $solution -BuildConfiguration Release-Net40 -Target Clean
+    New-Build -Solution $solution -BuildConfiguration Release-Net40 -Target Clean
 }
 
 Task CleanNet35 -Description "Cleans the output directory of the .NET 3.5 build configuration" {
-    New-Build $solution -BuildConfiguration Release-Net35 -Target Clean
+    New-Build -Solution $solution -BuildConfiguration Release-Net35 -Target Clean
 }
 
 Task CleanDocs -Description "Cleans the output directory of the documentation build configuration" {
@@ -28,7 +28,7 @@ Task CleanDocs -Description "Cleans the output directory of the documentation bu
         $env:SHFBROOT = $PSScriptRoot + "\..\..\packages\SHFB.2014.5.31\tools\Sandcastle Help File Builder"
     }
     Try {
-        New-Build $solution -BuildConfiguration Docs -Target Clean
+        New-Build -Solution $solution -BuildConfiguration Docs -Target Clean
     } Finally {
         If ($envVarNotDefined) {
             $env:SHFBROOT = $null
@@ -39,15 +39,15 @@ Task CleanDocs -Description "Cleans the output directory of the documentation bu
 Task Clean -Description "Cleans the output directory of all build configurations" -Depends CleanNet45, CleanNet40, CleanNet35, CleanDocs
 
 Task CompileNet45 -Description "Compiles the default .NET 4.5 build configuration" -Depends CleanNet45 {
-    New-Build $solution
+    New-Build -Solution $solution
 }
 
 Task CompileNet40 -Description "Compiles the .NET 4.0 build configuration" -Depends CleanNet40 {
-    New-Build $solution -BuildConfiguration Release-Net40
+    New-Build -Solution $solution -BuildConfiguration Release-Net40
 }
 
 Task CompileNet35 -Description "Compiles the .NET 3.5 build configuration" -Depends CleanNet35 {
-    New-Build $solution -BuildConfiguration Release-Net35
+    New-Build -Solution $solution -BuildConfiguration Release-Net35
 }
 
 Task Compile -Description "Compiles all of the build configurations" -Depends CompileNet45, CompileNet40, CompileNet35
@@ -58,7 +58,7 @@ Task Docs -Description "Compiles the documentation build configuration" -Depends
         $env:SHFBROOT = $PSScriptRoot + "\..\..\packages\SHFB.2014.5.31\tools\Sandcastle Help File Builder"
     }
     Try {
-        New-Build $solution -BuildConfiguration Docs
+        New-Build -Solution $solution -BuildConfiguration Docs
     } Finally {
         If ($envVarNotDefined) {
             $env:SHFBROOT = $null
@@ -67,53 +67,53 @@ Task Docs -Description "Compiles the documentation build configuration" -Depends
 }
 
 Task Test -Description "Runs the unit tests" -Depends CompileNet45 {
-    Test-Assembly $unitTests
+    Test-Assembly -Tests $unitTests
 }
 
 Task IntegrationPhantomJs -Description "Runs the PhantomJS integration tests" -Depends CompileNet45 {
-    Test-Assembly $integrationTests -Trait Browser=PhantomJS
+    Test-Assembly -Tests $integrationTests -Trait Browser=PhantomJS
 }
 
 Task IntegrationChrome -Description "Runs the Chrome integration tests" -Depends CompileNet45 {
-    Test-Assembly $integrationTests -Trait Browser=Chrome
+    Test-Assembly -Tests $integrationTests -Trait Browser=Chrome
 }
 
 Task IntegrationFirefox -Description "Runs the Firefox integration tests" -Depends CompileNet45 {
-    Test-Assembly $integrationTests -Trait Browser=Firefox
+    Test-Assembly -Tests $integrationTests -Trait Browser=Firefox
 }
 
 Task IntegrationInternetExplorer -Description "Runs the Internet Explorer integration tests" -Depends CompileNet45 {
-    Test-Assembly $integrationTests -Trait Browser=InternetExplorer
+    Test-Assembly -Tests $integrationTests -Trait Browser=InternetExplorer
 }
 
 Task Integration -Description "Runs all of the integration tests" -Depends IntegrationPhantomJs, IntegrationChrome, IntegrationFirefox, IntegrationInternetExplorer
 
 Task AnalyzeCoverage -Description "Analyzes the code coverage" -Depends CompileNet45 {
-    New-CoverageAnalysis $unitTests $coverageXml
+    New-CoverageAnalysis -Tests $unitTests -Output $coverageXml
 }
 
 Task Coverage -Description "Generates the code coverage HTML report" -Depends AnalyzeCoverage {
-    New-CoverageReport $coverageXml .\CoverageReport
+    New-CoverageReport -CoverageXml $coverageXml -Output .\CoverageReport
 }
 
 Task Coveralls -Description "Sends coverage data to coveralls.io" -Depends AnalyzeCoverage {
-    Publish-Coveralls $coverageXml
+    Publish-Coveralls -CoverageXml $coverageXml
 }
 
 Task PackJQuery -Description "Packs JQuery module NuGet package" -Depends Compile {
-    New-NugetPackage .\src\Selenium.WebDriver.Extensions.JQuery\Selenium.WebDriver.Extensions.JQuery.nuspec -Version $version
+    New-NugetPackage -Specification .\src\Selenium.WebDriver.Extensions.JQuery\Selenium.WebDriver.Extensions.JQuery.nuspec -Version $version
 }
 
 Task PackSizzle -Description "Packs Sizzle module NuGet package" -Depends Compile {
-    New-NugetPackage .\src\Selenium.WebDriver.Extensions.Sizzle\Selenium.WebDriver.Extensions.Sizzle.nuspec -Version $version
+    New-NugetPackage -Specification .\src\Selenium.WebDriver.Extensions.Sizzle\Selenium.WebDriver.Extensions.Sizzle.nuspec -Version $version
 }
 
 Task PackCore -Description "Packs core module NuGet package" -Depends Compile {
-    New-NugetPackage .\src\Selenium.WebDriver.Extensions.Core\Selenium.WebDriver.Extensions.Core.nuspec -Version $version
+    New-NugetPackage -Specification .\src\Selenium.WebDriver.Extensions.Core\Selenium.WebDriver.Extensions.Core.nuspec -Version $version
 }
 
 Task PackCombined -Description "Packs combined module NuGet package" -Depends Compile {
-    New-NugetPackage .\src\Selenium.WebDriver.Extensions\Selenium.WebDriver.Extensions.nuspec -Version $version
+    New-NugetPackage -Specification .\src\Selenium.WebDriver.Extensions\Selenium.WebDriver.Extensions.nuspec -Version $version
 }
 
 Task Pack -Description "Packs all of the module NuGet packages" -Depends PackJQuery, PackSizzle, PackCore, PackCombined
