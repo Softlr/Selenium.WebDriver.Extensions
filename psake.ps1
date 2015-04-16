f15c0671619f08972e027e4efa0cc484eaf859ff
Lukasz Rajchel <lukasz@rajchel.pl>
2015-04-16 08:23:35 +0200

    psake build module references via nuget package [skip ci]

--- psake.ps1
+++ psake.ps1
@@ -1,7 +1,7 @@
 $root = Split-Path $script:MyInvocation.MyCommand.Path -Parent
-& $root/.nuget/NuGet.exe restore $root/Selenium.WebDriver.Extensions.sln
+& $root\.nuget\NuGet.exe restore $root/Selenium.WebDriver.Extensions.sln
 Import-Module $root\packages\psake.4.4.1\tools\psake.psm1
-Import-Module $root\build.psm1
+Import-Module $root\packages\psake.net.build.module.1.0.6\tools\build.psm1
 Invoke-psake $root\default.ps1 @args
 Remove-Module build
 Remove-Module psake
