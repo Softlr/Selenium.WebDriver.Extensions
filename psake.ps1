0e6a06775e91d51c6ab2c29c887a267e3cdbef15
Lukasz Rajchel <lukasz@rajchel.pl>
2015-07-01 21:11:40 +0200

    packages updated

--- psake.ps1
+++ psake.ps1
@@ -1,7 +1,9 @@
 $root = Split-Path $script:MyInvocation.MyCommand.Path -Parent
 & $root\.nuget\NuGet.exe restore $root/Selenium.WebDriver.Extensions.sln
-Import-Module $root\packages\psake.4.4.1\tools\psake.psm1
-Import-Module $root\packages\psake.net.build.module.1.0.6\tools\build.psm1
+$psakePath = Resolve-Path $root\packages\psake.*\tools\psake.psm1
+Import-Module $psakePath
+$psakeBuildPath = Resolve-Path $root\packages\psake.net.build.module.*\tools\build.psm1
+Import-Module $psakeBuildPath
 Invoke-psake $root\default.ps1 @args
 Remove-Module build
 Remove-Module psake
