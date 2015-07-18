bd19d2f4ac5f0ce79cad037bbebba488b540dbe8
Lukasz Rajchel <lukasz@rajchel.pl>
2015-07-18 10:20:20 +0200

    build scipts updated for V2

--- src/Selenium.WebDriver.Extensions/Extensions/StringExtensions.cs
+++ src/Selenium.WebDriver.Extensions/Extensions/StringExtensions.cs
@@ -12,7 +12,9 @@
         /// <returns>
         /// <c>true</c> if the value parameter is <c>null</c> or an empty string (<c>""</c>); otherwise, <c>false</c>.
         /// </returns>
-        /// <remarks>This method is a implementation for <see cref="string.IsNullOrWhiteSpace"/> for .NET 3.5.</remarks>
+        /// <remarks>
+        /// This method is a implementation for <c>IsNullOrWhiteSpace</c> for <see cref="string"/> for .NET 3.5.
+        /// </remarks>
         public static bool IsNullOrWhiteSpace(this string value)
         {
             return value == null || string.IsNullOrEmpty(value.Trim());
