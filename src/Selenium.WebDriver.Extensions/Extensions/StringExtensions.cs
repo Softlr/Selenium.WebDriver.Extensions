b10b189b639f745fe12d3fba08a35293fde9092a
Lukasz Rajchel <lukasz@rajchel.pl>
2016-08-12 08:53:55 +0200

    documentation fixes

--- src/Selenium.WebDriver.Extensions/Extensions/StringExtensions.cs
+++ src/Selenium.WebDriver.Extensions/Extensions/StringExtensions.cs
@@ -1,20 +1,20 @@
 ﻿namespace OpenQA.Selenium.Extensions
 {
     /// <summary>
-    /// The string extensions.
+    /// The class containing the extension methods for the <see cref="string"> class.
     /// </summary>
     public static class StringExtensions
     {
         /// <summary>
-        /// Indicates whether the specified string is <see langword="null"/> or an empty string.
+        /// Indicates whether the specified <paramref name="value"/> is <see langword="null"/> or an empty string.
         /// </summary>
-        /// <param name="value">The string to test. </param>
+        /// <param name="value">The string to test.</param>
         /// <returns>
         /// <see langword="true"/> if the value parameter is <see langword="null"/> or an empty string (<c>""</c>);
         /// otherwise, <see langword="false"/>.
         /// </returns>
         /// <remarks>
-        /// This method is a implementation for <see cref="IsNullOrWhiteSpace"/> for <see cref="string"/> for .NET 3.5.
+        /// This method is the implementation of <see cref="IsNullOrWhiteSpace"/> compatible with .NET 3.5.
         /// </remarks>
         public static bool IsNullOrWhiteSpace(this string value) => string.IsNullOrEmpty(value?.Trim());
     }
