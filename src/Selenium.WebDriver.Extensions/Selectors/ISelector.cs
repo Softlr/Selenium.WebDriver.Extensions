98884dba81ac3e82bc6fc996d460999dbf802e5b
Lukasz Rajchel <lukasz@rajchel.pl>
2016-08-15 20:11:56 +0200

    unnecessary usings removed

--- src/Selenium.WebDriver.Extensions/Selectors/ISelector.cs
+++ src/Selenium.WebDriver.Extensions/Selectors/ISelector.cs
@@ -1,10 +1,6 @@
 ﻿namespace OpenQA.Selenium
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     /// <summary>
     /// The selector inteface.
