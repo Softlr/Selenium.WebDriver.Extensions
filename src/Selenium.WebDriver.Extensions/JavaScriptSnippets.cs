06e405f581766b2844c4da479f8a2b340db2f3b5
Lukasz Rajchel <lukasz@rajchel.pl>
2016-08-22 12:11:14 +0200

    more expression-bodied member added

--- src/Selenium.WebDriver.Extensions/JavaScriptSnippets.cs
+++ src/Selenium.WebDriver.Extensions/JavaScriptSnippets.cs
@@ -1,7 +1,6 @@
-﻿namespace OpenQA.Selenium
+namespace OpenQA.Selenium
 {
     using System;
-    using PostSharp.Patterns.Contracts;
 
     /// <summary>
     /// The external library loader base.
@@ -11,7 +10,7 @@
         /// <summary>
         /// Gets the script to get the DOM path.
         /// </summary>
-        public static string FindDomPathScript { get; } = @"return (function(element) {
+        internal static string FindDomPathScript { get; } = @"return (function(element) {
             'use strict';
             var stack = [], siblingsCount, siblingIndex, i, sibling;
             while (element.parentNode !== null) {
@@ -37,36 +36,28 @@
             }
             stack = stack.slice(1);
             return stack.join(' > ');
-            })(arguments[0]);";
+        })(arguments[0]);";
 
         /// <summary>
         /// Gets the JavaScript to load jQuery.
         /// </summary>
         /// <param name="url">The script URL.</param>
         /// <returns>The JavaScrpt to load URL.</returns>
-        public static string LoadScriptCode([Required] Uri url)
-        {
-            const string script = @"(function(source) {
-                'use strict';
-                var script = document.createElement('script');
-                script.src = source;
-                document.getElementsByTagName('body')[0].appendChild(script);
-            })";
-            return $"{script}('{url}')";
-        }
+        internal static string LoadScriptCode(Uri url) => $@"(function(source) {{
+            'use strict';
+            var script = document.createElement('script');
+            script.src = source;
+            document.getElementsByTagName('body')[0].appendChild(script);
+        }})('{url}')";
 
         /// <summary>
         /// Gets the JavaScript to test if library variable is defined.
         /// </summary>
         /// <param name="variable">The library variable to test.</param>
         /// <returns>The JavaScrpt to test if library variable is defined.</returns>
-        public static string CheckScriptCode([Required] string variable)
-        {
-            const string script = @"(function(value) {
-                'use strict';
-                return typeof value === 'function';
-            })";
-            return $"{script}({variable})";
-        }
+        internal static string CheckScriptCode(string variable) => $@"(function(value) {{
+            'use strict';
+            return typeof value === 'function';
+        }})({variable})";
     }
 }
