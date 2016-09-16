c692b3f21565f73dfca8712e86e472dbf709e8cf
Lukasz Rajchel <lukasz@rajchel.pl>
2016-09-16 09:34:28 +0200

    fixes #152, fixes #147

--- build/New-NuGetPackage.ps1
+++ build/New-NuGetPackage.ps1
@@ -1,64 +1,72 @@
-<#
-.SYNOPSIS
-Create the NuGet package based on the given specification.
+Function New-NugetPackage {
+	<#
+	.SYNOPSIS
+		Create the NuGet package based on the given specification.
 
-.DESCRIPTION
-This function creates the NuGet package based on the given NuGet specification files.
+	.DESCRIPTION
+		This function creates the NuGet package based on the given NuGet specification files.
 
-.PARAMETER Specification
-The paths for the NuGet specification files.
+	.PARAMETER Path
+		The paths for the NuGet specification files.
 
-.PARAMETER Output
-The output directory for NuGet package.
+	.PARAMETER Destination
+		The destination for NuGet package.
 
-.PARAMETER Version
-The version of the package.
+	.PARAMETER Version
+		The version of the package.
 
-.EXAMPLE
-New-NugetPackage -Specification .\Foo.nuspec
+	.EXAMPLE
+		New-NugetPackage -Path .\Foo.nuspec
 
-This example shows how to create the new NuGet package for the given NuGet specification.
+		This example shows how to create the new NuGet package for the given NuGet specification.
 
-The Specification parameter name (-Specification) is optional and can be omitted.
+	.EXAMPLE
+		New-NugetPackage -Path .\Foo.nuspec, .\Bar.nuspec
 
-.EXAMPLE
-New-NugetPackage -Specification .\Foo.nuspec , .\Bar.nuspec
+		This example shows how to create the new NuGet packages for the multiple NuGet specifications. The specifications are 
+		passed as a string array and are separated by comma (,).
 
-This example shows how to create the new NuGet packages for the multiple NuGet specifications. The specifications are 
-passed as a string array and are separated by comma (,).
+	.EXAMPLE
+		New-NugetPackage -Path .\Foo.nuspec -Destination .\packages
 
-The Specification parameter name (-Specification) is optional and can be omitted.
+		This example shows how to create the new NuGet package for the given NuGet specification and saving them in the given 
+		location.
 
-.EXAMPLE
-New-NugetPackage -Specification .\Foo.nuspec -Output .\packages
+	.EXAMPLE
+		New-NugetPackage -Path .\Foo.nuspec -Version 2.0
 
-This example shows how to create the new NuGet package for the given NuGet specification and saving them in the given 
-location.
-
-The Specification parameter name (-Specification) is optional and can be omitted.
-
-.EXAMPLE
-New-NugetPackage -Specification .\Foo.nuspec -Version 2.0
-
-This example shows how to create the new NuGet package for the given NuGet specification and replacing the version 
-token in the specifications prior to the package creation.
-
-The Specification parameter name (-Specification) is optional and can be omitted.
-#>
-function New-NugetPackage {
+		This example shows how to create the new NuGet package for the given NuGet specification and replacing the version 
+		token in the specifications prior to the package creation.
+	#>
+	[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
     [CmdletBinding()]
-    param(
+	[OutputType([void])]
+    Param(
         [Parameter(Mandatory = $true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
         [ValidateNotNullOrEmpty()]
-        [string[]] $Specification,
+        [string[]] $Path,
 
-        [string] $Output = '.',
+        [string] $Destination = '.',
 
         [string] $Version = '1.0'
     )
+	
+	Begin {
+        $nugetPath = Get-Location | Join-Path -ChildPath .nuget | Join-Path -ChildPath NuGet.exe
+        If ($nugetPath | Test-Path) {
+            $nuget = $nugetPath | Resolve-Path
+        } Else {
+            Throw 'NuGet.exe not found'
+        }
+    }
 
-    Exec {
-        $Specification `
-        | ForEach-Object -Process { ./.nuget/NuGet.exe pack $_ -Version $Version -OutputDirectory $Output -Symbols }
+    Process {
+		$Path | ForEach-Object -Process {
+			$arguments = 'pack', $_,  '-OutputDirectory', $Destination, '-Version', $Version, '-Symbols'
+			$process = Start-Process -FilePath $nuget -ArgumentList $arguments -NoNewWindow -Wait -PassThru
+            If ($process.ExitCode) {
+                Throw 'NuGet package generation failed'
+            }
+        }
     }
 }
--- default.ps1
+++ default.ps1
@@ -1,6 +1,6 @@
 Properties {
 	$currentDir = '.' | Resolve-Path
-    $solution = $currentDir | Join-Path -ChildPath *.sln | Resolve-Path
+    $solution = $currentDir | Join-Path -ChildPath Selenium.WebDriver.Extensions.sln
     $version = '3.0.0'
     $unitTests = $currentDir | Join-Path -ChildPath test | Join-Path -ChildPath Selenium.WebDriver.Extensions.Tests `
 		| Join-Path -ChildPath bin | Join-Path -ChildPath Release | Join-Path -ChildPath Selenium.WebDriver.Extensions.Tests.dll
@@ -13,7 +13,7 @@ Properties {
 
 FormatTaskName '-------- {0} --------'
 
-Task default -Depends Clean, Compile, Test, Coverage, Docs, Pack
+Task Default -Depends Clean, Build, Test, Coverage, Docs, Pack
 
 Task CleanArtifacts -Description 'Cleans the artifacts directory' {
     If (Test-Path -Path $artifactsDir) {
@@ -23,15 +23,15 @@ Task CleanArtifacts -Description 'Cleans the artifacts directory' {
 }
 
 Task CleanNet46 -Description 'Cleans the output directory of the default .NET 4.6 build configuration' {
-    New-Build -Solution $solution -Target Clean
+    Invoke-Build -Path $solution -Target Clean
 }
 
 Task CleanNet45 -Description 'Cleans the output directory of the default .NET 4.5 build configuration' {
-    New-Build -Solution $solution -BuildConfiguration Release-Net45 -Target Clean
+    Invoke-Build -Path $solution -BuildConfiguration Release-Net45 -Target Clean
 }
 
 Task CleanNet40 -Description 'Cleans the output directory of the .NET 4.0 build configuration' {
-    New-Build -Solution $solution -BuildConfiguration Release-Net40 -Target Clean
+    Invoke-Build -Path $solution -BuildConfiguration Release-Net40 -Target Clean
 }
 
 Task CleanDocs -Description 'Cleans the output directory of the documentation build configuration' {
@@ -40,7 +40,7 @@ Task CleanDocs -Description 'Cleans the output directory of the documentation bu
         $env:SHFBROOT = $PWD.Path + '\packages\EWSoftware.SHFB.2015.10.10.0\tools'
     }
     Try {
-        New-Build -Solution $solution -BuildConfiguration Docs -Target Clean
+        Invoke-Build -Path $solution -BuildConfiguration Docs -Target Clean
     } Finally {
         If ($envVarNotDefined) {
             $env:SHFBROOT = $null
@@ -51,66 +51,63 @@ Task CleanDocs -Description 'Cleans the output directory of the documentation bu
 Task Clean -Description 'Cleans the output directory of all build configurations' `
 	-Depends CleanNet46, CleanNet45, CleanNet40, CleanDocs, CleanArtifacts
 
-Task CompileNet46 -Description 'Compiles the default .NET 4.6 build configuration' -Depends CleanNet46 {
-    New-Build -Solution $solution
+Task BuildNet46 -Description 'Builds the default .NET 4.6 build configuration' -Depends CleanNet46 {
+    Invoke-Build -Path $solution
 }
 
-Task CompileNet45 -Description 'Compiles the default .NET 4.5 build configuration' -Depends CleanNet45 {
-    New-Build -Solution $solution -BuildConfiguration Release-Net45
+Task BuildNet45 -Description 'Builds the default .NET 4.5 build configuration' -Depends CleanNet45 {
+    Invoke-Build -Path $solution -BuildConfiguration Release-Net45
 }
 
-Task CompileNet40 -Description 'Compiles the .NET 4.0 build configuration' -Depends CleanNet40 {
-    New-Build -Solution $solution -BuildConfiguration Release-Net40
+Task BuildNet40 -Description 'Builds the .NET 4.0 build configuration' -Depends CleanNet40 {
+    Invoke-Build -Path $solution -BuildConfiguration Release-Net40
 }
 
-Task Compile -Description 'Compiles all of the build configurations' -Depends CompileNet46, CompileNet45, CompileNet40
+Task Build -Description 'Builds all of the build configurations' -Depends BuildNet46, BuildNet45, BuildNet40
 
-Task Docs -Description 'Compiles the documentation build configuration' -Depends CleanDocs, CleanArtifacts, CompileNet46 {
-    New-Build -Solution $solution -BuildConfiguration Docs
+Task Docs -Description 'Builds the documentation build configuration' -Depends CleanDocs, CleanArtifacts, BuildNet46 {
+    Invoke-Build -Path $solution -BuildConfiguration Docs
     
 	$docsDir = $currentDir | Join-Path -ChildPath Docs | Join-Path -ChildPath bin | Join-Path -ChildPath Docs
     Move-Item -Path $docsDir -Destination $artifactsDir
 }
 
-Task Test -Description 'Runs the unit tests' -Depends CompileNet46 {
-    Test-Assembly -Tests $unitTests
+Task Test -Description 'Runs the unit tests' -Depends BuildNet46 {
+    Invoke-Test -Path $unitTests
 }
 
-Task IntegrationPhantomJs -Description 'Runs the PhantomJS integration tests' -Depends CompileNet46 {
-    Test-Assembly -Tests $integrationTests -Trait Browser=PhantomJS
+Task IntegrationPhantomJs -Description 'Runs the PhantomJS integration tests' -Depends BuildNet46 {
+    Invoke-Test -Path $integrationTests -Trait Browser=PhantomJS
 }
 
-Task IntegrationChrome -Description 'Runs the Chrome integration tests' -Depends CompileNet46 {
-    Test-Assembly -Tests $integrationTests -Trait Browser=Chrome
+Task IntegrationChrome -Description 'Runs the Chrome integration tests' -Depends BuildNet46 {
+    Invoke-Test -Path $integrationTests -Trait Browser=Chrome
 }
 
-Task IntegrationFirefox -Description 'Runs the Firefox integration tests' -Depends CompileNet46 {
-    Test-Assembly -Tests $integrationTests -Trait Browser=Firefox
+Task IntegrationFirefox -Description 'Runs the Firefox integration tests' -Depends BuildNet46 {
+    Invoke-Test -Path $integrationTests -Trait Browser=Firefox
 }
 
-Task IntegrationInternetExplorer -Description 'Runs the Internet Explorer integration tests' -Depends CompileNet46 {
-    Test-Assembly -Tests $integrationTests -Trait Browser=InternetExplorer
+Task IntegrationInternetExplorer -Description 'Runs the Internet Explorer integration tests' -Depends BuildNet46 {
+    Invoke-Test -Path $integrationTests -Trait Browser=InternetExplorer
 }
 
-Task IntegrationEdge -Description 'Runs the Internet Explorer integration tests' -Depends CompileNet46 {
-    Test-Assembly -Tests $integrationTests -Trait Browser=Edge
+Task IntegrationEdge -Description 'Runs the Internet Explorer integration tests' -Depends BuildNet46 {
+    Invoke-Test -Path $integrationTests -Trait Browser=Edge
 }
 
 Task Integration -Description 'Runs all of the integration tests' `
 	-Depends IntegrationPhantomJs, IntegrationChrome, IntegrationFirefox, IntegrationInternetExplorer, IntegrationEdge
 
-Task AnalyzeCoverage -Description 'Analyzes the code coverage' -Depends CompileNet46 {
-    New-CoverageAnalysis -Tests $unitTests -Output $coverageXml `
-		-Filter '+[Selenium.WebDriver.Extensions*]* -[*]*Exception* -[*Tests]* -[xunit*]*'
+Task Coverage -Description 'Generates the code coverage HTML report' -Depends BuildNet46 {
+	Invoke-Coverage -Path $unitTests -Destination $coverageXml `
+		-Filter '+[Selenium.WebDriver.Extensions*]* -[*]*Exception* -[*Tests]* -[xunit*]*'`
+		-ReportDestination ($artifactsDir | Join-Path -ChildPath CoverageReport)
 }
 
-Task Coverage -Description 'Generates the code coverage HTML report' -Depends AnalyzeCoverage {
-    New-CoverageReport -CoverageXml $coverageXml -Output ($artifactsDir | Join-Path -ChildPath CoverageReport)
-}
-
-Task Pack -Description 'Packs NuGet package' -Depends Compile {
+Task Pack -Description 'Packs NuGet package' -Depends Build {
 	$specPath = $currentDir | Join-Path -ChildPath src | Join-Path -ChildPath Selenium.WebDriver.Extensions `
 		| Join-Path -ChildPath *.nuspec | Resolve-Path
-    New-NugetPackage -Specification $specPath -Version $version
+    New-NugetPackage -Path $specPath -Version $version
     Move-Item -Path ($currentDir | Join-Path -ChildPath *.nupkg) -Destination $artifactsDir
 }
