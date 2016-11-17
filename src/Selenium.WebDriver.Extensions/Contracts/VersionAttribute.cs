0628f677095b5ecc1bd1ae5fbd7fdc9870234270
Lukasz Rajchel <lukasz.rajchel@softwareone.com>
2016-11-17 15:57:07 +0100

    #167 version attribute added

---
+++ src/Selenium.WebDriver.Extensions/Contracts/VersionAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace Selenium.WebDriver.Extensions.Contracts
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using PostSharp.Aspects;
+    using PostSharp.Patterns.Contracts;
+    using PostSharp.Reflection;
+
+    /// <summary>
+    /// The attribute to validate null or not-empty string.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public sealed class VersionAttribute : LocationContractAttribute, ILocationValidationAspect<string>
+    {
+        /// <inheritdoc/>
+        public Exception ValidateValue(string value, string locationName, LocationKind locationKind)
+        {
+            Version version;
+            return Version.TryParse(value, out version)
+                ? CreateArgumentException(value, locationName, locationKind)
+                : null;
+        }
+
+        /// <inheritdoc/>
+        protected override string GetErrorMessage() => "Argument is not a valid library version";
+    }
+}
