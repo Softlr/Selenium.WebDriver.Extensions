abfbfad00eee38d40c85f9d969ab04b379f2b7c6
RaYell <lukasz@rajchel.pl>
2018-01-10 23:04:48 +0100

    test HTML moved to const

--- test/Selenium.WebDriver.Extensions.IntegrationTests/TestCaseModule.cs
+++ test/Selenium.WebDriver.Extensions.IntegrationTests/TestCaseModule.cs
@@ -1,44 +1,71 @@
 namespace Selenium.WebDriver.Extensions.IntegrationTests
 {
     using System.Diagnostics.CodeAnalysis;
-    using System.IO;
-    using System.Reflection;
     using Nancy;
 
     [ExcludeFromCodeCoverage]
     public class TestCaseModule : NancyModule
     {
+        private const string JQUERY_LOADED = @"<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=""utf-8"" />
+    <title>Selenium.WebDriver.Extensions Tests</title>
+</head>
+<body>
+    <h1 class=""main"">H1 Header</h1>
+    <div class=""main"" id=""id1"">jQuery Selector</div>
+    <div class=""main"">Selenium WebDriver Extensions</div>
+    <script src = ""https://code.jquery.com/jquery-2.1.1.min.js"" type=""text/javascript""></script>
+</body>
+</html>";
+
+        private const string JQUERY_UNLOADED = @"<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=""utf-8"" />
+    <title>Selenium.WebDriver.Extensions Tests</title>
+</head>
+<body>
+    <h1 class=""main"">H1 Header</h1>
+    <div class=""main"" id=""id1"">jQuery Selector</div>
+    <div class=""main"">Selenium WebDriver Extensions</div>
+</body>
+</html>";
+
+        private const string SIZZLE_LOADED = @"<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=""utf-8"" />
+    <title>Selenium.WebDriver.Extensions Tests</title>
+</head>
+<body>
+    <h1 class=""main"">H1 Header</h1>
+    <div class=""main"" id=""id1"">Sizzle Selector</div>
+    <div class=""main"">Selenium WebDriver Extensions</div>
+    <script src = ""https://cdnjs.cloudflare.com/ajax/libs/sizzle/2.0.0/sizzle.min.js"" type=""text/javascript""></script>
+</body>
+</html>";
+
+        private const string SIZZLE_UNLOADED = @"<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=""utf-8"" />
+    <title>Selenium.WebDriver.Extensions Tests</title>
+</head>
+<body>
+    <h1 class=""main"">H1 Header</h1>
+    <div class=""main"" id=""id1"">Sizzle Selector</div>
+    <div class=""main"">Selenium WebDriver Extensions</div>
+</body>
+</html>";
+
         public TestCaseModule()
         {
-            const string prefix = "Selenium.WebDriver.Extensions.IntegrationTests.TestCases.";
-            Get["/jQueryLoaded"] = _ => GetHtml($"{prefix}JQuery.Loaded.html");
-            Get["/jQueryUnloaded"] = _ => GetHtml($"{prefix}JQuery.Unloaded.html");
-            Get["/SizzleLoaded"] = _ => GetHtml($"{prefix}Sizzle.Loaded.html");
-            Get["/SizzleUnloaded"] = _ => GetHtml($"{prefix}Sizzle.Unloaded.html");
-        }
-
-        private static string GetHtml(string resourceName)
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            Stream stream = null;
-            try
-            {
-                stream = assembly.GetManifestResourceStream(resourceName);
-                if (stream == null)
-                {
-                    return null;
-                }
-
-                using (var reader = new StreamReader(stream))
-                {
-                    stream = null;
-                    return reader.ReadToEnd();
-                }
-            }
-            finally
-            {
-                stream?.Dispose();
-            }
+            Get["/jQueryLoaded"] = _ => JQUERY_LOADED;
+            Get["/jQueryUnloaded"] = _ => JQUERY_UNLOADED;
+            Get["/SizzleLoaded"] = _ => SIZZLE_LOADED;
+            Get["/SizzleUnloaded"] = _ => SIZZLE_UNLOADED;
         }
     }
 }
