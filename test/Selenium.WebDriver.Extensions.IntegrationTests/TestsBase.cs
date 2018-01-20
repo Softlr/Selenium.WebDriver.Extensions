5690810c982ffdcbe47dd335f3165fc2a1f493ff
RaYell <lukasz@rajchel.pl>
2018-01-20 11:37:28 +0100

    Revert "postsharp disposable"
    This reverts commit 938c84de840699c5713659dee521b49312598bb4.

--- test/Selenium.WebDriver.Extensions.IntegrationTests/TestsBase.cs
+++ test/Selenium.WebDriver.Extensions.IntegrationTests/TestsBase.cs
@@ -2,32 +2,47 @@ namespace Selenium.WebDriver.Extensions.IntegrationTests
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
-    using JetBrains.Annotations;
     using Nancy.Hosting.Self;
     using OpenQA.Selenium;
-    using PostSharp.Patterns.Model;
 
-    [Disposable]
-    [PublicAPI]
     [ExcludeFromCodeCoverage]
-    public abstract class TestsBase
+    public abstract class TestsBase : IDisposable
     {
+        private readonly NancyHost _host;
+        private bool _disposed;
+
         protected TestsBase(IWebDriver browser, string path)
         {
             var config = new HostConfiguration { UrlReservations = { CreateAutomatically = true } };
 
             const string serverUrl = "http://localhost:50502";
-            Host = new NancyHost(config, new Uri(serverUrl));
-            Host.Start();
+            _host = new NancyHost(config, new Uri(serverUrl));
+            _host.Start();
 
             Browser = browser;
             Browser.Navigate().GoToUrl(new Uri($"{serverUrl}{path}"));
         }
 
-        [Child]
-        protected NancyHost Host { get; }
+        ~TestsBase() => Dispose(false);
 
-        [Reference]
         protected IWebDriver Browser { get; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        [SuppressMessage("ReSharper", "VirtualMemberNeverOverridden.Global")]
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed || !disposing)
+            {
+                return;
+            }
+
+            _host.Dispose();
+            _disposed = true;
+        }
     }
 }
