f6740447afd74253c74437b3d45fba7244dd5591
RaYell <lukasz@rajchel.pl>
2018-04-30 21:37:25 +0200

    access modifiers modified

--- src/Selenium.WebDriver.Extensions/Contracts/NullOrNotEmptyAttribute.cs
+++ src/Selenium.WebDriver.Extensions/Contracts/NullOrNotEmptyAttribute.cs
@@ -6,22 +6,16 @@ namespace Selenium.WebDriver.Extensions.Contracts
     using PostSharp.Patterns.Contracts;
     using PostSharp.Reflection;
 
-    /// <summary>
-    /// The attribute to validate null or not-empty string.
-    /// </summary>
-    /// <inheritdoc cref="LocationContractAttribute" />
     [ExcludeFromCodeCoverage]
     [AttributeUsage(AttributeTargets.Parameter)]
-    public sealed class NullOrNotEmptyAttribute : LocationContractAttribute, ILocationValidationAspect<string>
+    internal sealed class NullOrNotEmptyAttribute : LocationContractAttribute, ILocationValidationAspect<string>
     {
-        /// <inheritdoc/>
         public Exception ValidateValue(
             string value, string locationName, LocationKind locationKind, LocationValidationContext context) =>
             value == null || !string.IsNullOrEmpty(value.Trim())
                 ? null
                 : CreateArgumentException(value, locationName, locationKind);
 
-        /// <inheritdoc/>
         protected override string GetErrorMessage() => "Argument must be null or not-empty";
     }
 }
