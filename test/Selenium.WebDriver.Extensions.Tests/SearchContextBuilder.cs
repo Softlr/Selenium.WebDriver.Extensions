c1affc6a2ead5c5a5f09b63091e44705eee2b77f
RaYell <lukasz@rajchel.pl>
2021-02-05 21:25:49 +0100

    tests cleanup

--- test/Selenium.WebDriver.Extensions.Tests/SearchContextBuilder.cs
+++ test/Selenium.WebDriver.Extensions.Tests/SearchContextBuilder.cs
@@ -4,17 +4,18 @@ namespace Selenium.WebDriver.Extensions.Tests
     using NSubstitute;
     using OpenQA.Selenium;
     using OpenQA.Selenium.Internal;
+    using static NSubstitute.Substitute;
 
     [ExcludeFromCodeCoverage]
     internal class SearchContextBuilder
     {
         private ISearchContext _searchContext;
 
-        public SearchContextBuilder() => _searchContext = Substitute.For<ISearchContext, IWrapsDriver>();
+        public SearchContextBuilder() => _searchContext = For<ISearchContext, IWrapsDriver>();
 
         public SearchContextBuilder AsWebElement()
         {
-            _searchContext = Substitute.For<ISearchContext, IWrapsDriver, IWebElement>();
+            _searchContext = For<ISearchContext, IWrapsDriver, IWebElement>();
             return this;
         }
 
