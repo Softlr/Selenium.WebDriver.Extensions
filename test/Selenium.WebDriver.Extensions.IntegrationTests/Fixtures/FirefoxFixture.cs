4df8123275d2ac4a65898a71138c1139d8793804
RaYell <lukasz@rajchel.pl>
2021-10-06 23:12:42 +0200

    trying to improve the speed of Firefox tests

--- test/Selenium.WebDriver.Extensions.IntegrationTests/Fixtures/FirefoxFixture.cs
+++ test/Selenium.WebDriver.Extensions.IntegrationTests/Fixtures/FirefoxFixture.cs
@@ -1,6 +1,8 @@
 namespace Selenium.WebDriver.Extensions.IntegrationTests.Fixtures
 {
     using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.Text;
     using OpenQA.Selenium.Firefox;
 
     [ExcludeFromCodeCoverage]
@@ -8,9 +10,31 @@ namespace Selenium.WebDriver.Extensions.IntegrationTests.Fixtures
     {
         public FirefoxFixture()
         {
-            var service = FirefoxDriverService.CreateDefaultService();
-            service.Host = "::1";
-            Browser = new FirefoxDriver(service);
+            // .Net Core workaround #1: Slow Firefox webdriver
+            string projectFolder = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+            string geckoDriverDirectory = projectFolder + "\\netcoreapp3.1\\";
+            FirefoxDriverService geckoService =
+            FirefoxDriverService.CreateDefaultService(geckoDriverDirectory);
+            geckoService.Host = "::1";
+
+            var ffOptions = new FirefoxOptions();
+            ffOptions.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox\Firefox.exe";
+            ffOptions.AcceptInsecureCertificates = true;
+
+            // This profile will by-pass *ALL* credentials. Note that Chrome uses Internet Explorer settings, so it does not need this.
+            // You must pre-setup the profile, by launching Firefox and doing phone authentication
+            // The profile's location is: C:\Users\<windows logon>\AppData\Local\Mozilla\Firefox\Profiles
+            // Without this, if your AUT uses SSO, you will always get prompted for the PIN or Two factor authentication
+            FirefoxProfile profile = new FirefoxProfileManager().GetProfile("Selenium");
+            ffOptions.Profile = profile;
+
+            // DotNet Core workaround #2- Code page
+            // https://stackoverflow.com/questions/56802715/firefoxwebdriver-no-data-is-available-for-encoding-437
+            // https://stackoverflow.com/questions/50858209/system-notsupportedexception-no-data-is-available-for-encoding-1252
+            CodePagesEncodingProvider.Instance.GetEncoding(437);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            Browser = new FirefoxDriver(geckoService, ffOptions);
         }
     }
 }
