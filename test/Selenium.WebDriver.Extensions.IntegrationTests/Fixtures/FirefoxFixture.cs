cb1b348463b7471d55a9679cbdc54d84e7b0a032
RaYell <lukasz@rajchel.pl>
2022-01-13 00:09:00 +0100

    sonarcloud issue resolved

--- test/Selenium.WebDriver.Extensions.IntegrationTests/Fixtures/FirefoxFixture.cs
+++ test/Selenium.WebDriver.Extensions.IntegrationTests/Fixtures/FirefoxFixture.cs
@@ -7,12 +7,14 @@ namespace Selenium.WebDriver.Extensions.IntegrationTests.Fixtures
     [ExcludeFromCodeCoverage]
     public class FirefoxFixture : FixtureBase
     {
+        private const int TIMEOUT_MINUTES = 3;
+
         public FirefoxFixture()
         {
             var options = new FirefoxOptions();
             options.AddArguments("--headless");
             Browser = new FirefoxDriver(
-                FirefoxDriverService.CreateDefaultService(), options, TimeSpan.FromMinutes(3));
+                FirefoxDriverService.CreateDefaultService(), options, TimeSpan.FromMinutes(TIMEOUT_MINUTES));
         }
     }
 }
